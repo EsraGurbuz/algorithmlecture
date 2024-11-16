51afb897294a0f3f389104fd6f298016bef48d5d
EsraGurbuz <esragurbuze@gmail.com>
2024-11-16 17:44:26 +0300

    GurbuzEsraLab5Ex1.java

--- GurbuzEsraLab5Ex1.java
+++ GurbuzEsraLab5Ex1.java
@@ -1,3 +1,12 @@
+/*******************************************************************
+@Title:	    GurbuzEsraAlgorithm&ProgrammingOddEven
+@Purpose:	To calculate the even and odd number
+@Author:    Gurbuz Esra
+@Date:   	11.17.2024 17th of November
+@Version:	1.0
+********************************************************************/
+
+
 import java.util.Scanner;
 
 public class GurbuzEsraLab5Ex1{
@@ -7,13 +16,14 @@ public class GurbuzEsraLab5Ex1{
 		System.out.println("Enter a positive integer");
 
 		int input = keyboard.nextInt();
-		int decision = input % 2;
 
 		while (input < 0) {
 			System.out.println("You entered an incorrect value.");
 			System.out.println("Re-enter please!");
 			input = keyboard.nextInt();
 		}
+
+		int decision = input % 2;
 
 		if(decision == 0) {
 			System.out.println("You entered an even value.");
