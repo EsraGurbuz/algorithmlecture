d604979551507250889618337dd0d6e81139bef4
EsraGurbuz <esragurbuze@gmail.com>
2024-11-16 17:01:27 +0300

    Add files via upload

---
+++ GurbuzEsraLab5Ex1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GurbuzEsraLab5Ex1{
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+
+		System.out.println("Enter a positive integer");
+
+		int input = keyboard.nextInt();
+		int decision = input % 2;
+
+		while (input < 0) {
+			System.out.println("You entered an incorrect value.");
+			System.out.println("Re-enter please!");
+		}
+
+		if(decision == 0) {
+			System.out.println("You entered an even value.");
+		} else {
+			System.out.println("You entered an odd value.");
+		}
+	}
+}
